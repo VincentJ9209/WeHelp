af4f3bc825cecfffc939f653ed1f7724356d00c3
VincentJ9209 <vincent.sj9209@gmail.com>
2024-12-05 16:27:08 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,121 @@
+alert("彭彭老師好，我叫張振綸 可以叫我Vincent")
+
+let aboutQ1 = document.querySelector("#Q1");
+let aboutQ2 = document.querySelector("#Q2");
+let aboutQ3 = document.querySelector("#Q3");
+let aboutQ4 = document.querySelector("#Q4");
+let aboutQ5 = document.querySelector("#Q5");
+let aboutQ6 = document.querySelector("#Q6");
+let aboutQ7 = document.querySelector("#Q7");
+let aboutQ8 = document.querySelector("#Q8");
+let aboutQ9 = document.querySelector("#Q9");
+
+function changeToQ1() {
+    aboutQ1.style.display = "block";
+    aboutQ2.style.display = "none";
+    aboutQ3.style.display = "none";
+    aboutQ4.style.display = "none";
+    aboutQ5.style.display = "none";
+    aboutQ6.style.display = "none";
+    aboutQ7.style.display = "none";
+    aboutQ8.style.display = "none";
+    aboutQ9.style.display = "none";
+}
+
+function changeToQ2() {
+    aboutQ1.style.display = "none";
+    aboutQ2.style.display = "block";
+    aboutQ3.style.display = "none";
+    aboutQ4.style.display = "none";
+    aboutQ5.style.display = "none";
+    aboutQ6.style.display = "none";
+    aboutQ7.style.display = "none";
+    aboutQ8.style.display = "none";
+    aboutQ9.style.display = "none";
+}
+
+
+function changeToQ3() {
+    aboutQ1.style.display = "none";
+    aboutQ2.style.display = "none";
+    aboutQ3.style.display = "block";
+    aboutQ4.style.display = "none";
+    aboutQ5.style.display = "none";
+    aboutQ6.style.display = "none";
+    aboutQ7.style.display = "none";
+    aboutQ8.style.display = "none";
+    aboutQ9.style.display = "none";
+}
+
+
+function changeToQ4() {
+    aboutQ1.style.display = "none";
+    aboutQ2.style.display = "none";
+    aboutQ3.style.display = "none";
+    aboutQ4.style.display = "block";
+    aboutQ5.style.display = "none";
+    aboutQ6.style.display = "none";
+    aboutQ7.style.display = "none";
+    aboutQ8.style.display = "none";
+    aboutQ9.style.display = "none";
+}
+
+function changeToQ5() {
+    aboutQ1.style.display = "none";
+    aboutQ2.style.display = "none";
+    aboutQ3.style.display = "none";
+    aboutQ4.style.display = "none";
+    aboutQ5.style.display = "block";
+    aboutQ6.style.display = "none";
+    aboutQ7.style.display = "none";
+    aboutQ8.style.display = "none";
+    aboutQ9.style.display = "none";
+}
+
+function changeToQ6() {
+    aboutQ1.style.display = "none";
+    aboutQ2.style.display = "none";
+    aboutQ3.style.display = "none";
+    aboutQ4.style.display = "none";
+    aboutQ5.style.display = "none";
+    aboutQ6.style.display = "block";
+    aboutQ7.style.display = "none";
+    aboutQ8.style.display = "none";
+    aboutQ9.style.display = "none";
+}
+
+function changeToQ7() {
+    aboutQ1.style.display = "none";
+    aboutQ2.style.display = "none";
+    aboutQ3.style.display = "none";
+    aboutQ4.style.display = "none";
+    aboutQ5.style.display = "none";
+    aboutQ6.style.display = "none";
+    aboutQ7.style.display = "block";
+    aboutQ8.style.display = "none";
+    aboutQ9.style.display = "none";
+}
+
+function changeToQ8() {
+    aboutQ1.style.display = "none";
+    aboutQ2.style.display = "none";
+    aboutQ3.style.display = "none";
+    aboutQ4.style.display = "none";
+    aboutQ5.style.display = "none";
+    aboutQ6.style.display = "none";
+    aboutQ7.style.display = "none";
+    aboutQ8.style.display = "block";
+    aboutQ9.style.display = "none";
+}
+
+function changeToQ9() {
+    aboutQ1.style.display = "none";
+    aboutQ2.style.display = "none";
+    aboutQ3.style.display = "none";
+    aboutQ4.style.display = "none";
+    aboutQ5.style.display = "none";
+    aboutQ6.style.display = "none";
+    aboutQ7.style.display = "none";
+    aboutQ8.style.display = "none";
+    aboutQ9.style.display = "block";
+}
